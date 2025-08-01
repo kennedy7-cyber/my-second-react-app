d1704d3a3a4801de668194fe48e442bf0d0a64a8
kennedy <adichronix@kali.andy>
2025-08-01 08:17:09 +0300

    fixed responsiveness of the carousels in all screens

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,151 +1,129 @@
 import React from "react";
-import { Link } from "react-router-dom"; // Make sure you're using react-router-dom
+import { Link } from "react-router-dom";
+
+const carouselSlides = [
+  {
+    img: "https://media.istockphoto.com/id/1943217822/photo/plumbers-tool-bag-on-kitchen-counter-near-sink-space-for-text.jpg?s=612x612&w=0&k=20&c=8rGIWM7usCVJhU4sHkhR3Y8xLUhG6QxtDlFEoJWc1RU=",
+
+    alt: "carpentry-workshop",
+    title: "Welcome to our solutions",
+    text: `We’re delighted to introduce you to our suite of services. From innovative strategies to seamless support, we’re here to empower your success. Let’s explore how we can make a meaningful impact together.`,
+    interval: 10000,
+    boldText: false,
+  },
+  {
+    img: "https://media.istockphoto.com/id/1138365767/photo/paintbrushes-of-different-size-and-paint-can.jpg?s=612x612&w=0&k=20&c=SyA25O8xBGnR92tfblPS94LcHI8M1TnU5YDVBwulyaI=",
+
+    alt: "construction-mall",
+    title: "Reliable services",
+    text: "“Dependable quality, every time. Your satisfaction is our guarantee.”",
+    interval: 2000,
+    boldText: true,
+  },
+  {
+    img: "https://media.istockphoto.com/id/821134188/photo/construction-iron.jpg?s=612x612&w=0&k=20&c=ocX4GvLL3o6p2uVwJd51_VSPx_kec6SdNrn8bVf-JL4=",
+    alt: "construction-framing",
+    title: "Smart & Scalable",
+    text: "Designed with intelligence, built to grow — delivering flexible solutions today and evolving seamlessly with tomorrow’s ambitions.",
+    interval: null,
+    boldText: false,
+  },
+];
 
 const Homepage = () => {
   return (
-    <div className="overflow-hidden">
-      <div className="d-block w-100 vh-100 h-100 overflow-hidden">
-        <div id="carouselExampleDark" class="carousel carousel-dark slide">
-          <div class="carousel-indicators">
-            <button
-              type="button"
-              data-bs-target="#carouselExampleDark"
-              data-bs-slide-to="0"
-              class="active"
-              aria-current="true"
-              aria-label="Slide 1"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleDark"
-              data-bs-slide-to="1"
-              aria-label="Slide 2"
-            ></button>
-            <button
-              type="button"
-              data-bs-target="#carouselExampleDark"
-              data-bs-slide-to="2"
-              aria-label="Slide 3"
-            ></button>
-          </div>
-          <div class="carousel-inner">
-            <div class="carousel-item active" data-bs-interval="10000">
-              <img
-                src="https://media.istockphoto.com/id/2162666018/photo/hand-tools-and-workbench-in-carpentry-workshop.webp?a=1&b=1&s=612x612&w=0&k=20&c=Ek1Qw0yW_qr6SLFUFQ6RP8uTw_4XMk1breJNJIbpEzs="
-                class="d-block w-100"
-                alt="..."
-              />
-              <div class="carousel-caption d-none d-md-block">
-                <h5>First slide label</h5>
-                <p>
-                  Some representative placeholder content for the first slide.
-                </p>
-              </div>
-            </div>
-            <div class="carousel-item" data-bs-interval="2000">
-              <img
-                src="https://media.istockphoto.com/id/1840468645/photo/tool-display-at-a-hardware-store.webp?a=1&b=1&s=612x612&w=0&k=20&c=kWm8_7Eyj8YdfHeZEde5cx1EUtRbMBog_Q6WgLfbJnM="
-                class="d-block w-100"
-                alt="..."
-              />
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Second slide label</h5>
-                <p>
-                  Some representative placeholder content for the second slide.
-                </p>
-              </div>
-            </div>
-            <div class="carousel-item">
-              <img
-                src="
-                https://media.istockphoto.com/id/1646081497/photo/young-woman-client-choosing-sanitary-taps-standing-with-catalog-near-the-showcase-of-plumbing.webp?a=1&b=1&s=612x612&w=0&k=20&c=2qEfV5PwWN4UI8WpJlayaXOx3HfDTl868uq3sBGx9Hg="
-                class="d-block w-100"
-                alt="..."
-              />
-              <div class="carousel-caption d-none d-md-block">
-                <h5>Third slide label</h5>
-                <p>
-                  Some representative placeholder content for the third slide.
-                </p>
-              </div>
-            </div>
-          </div>
-          <button
-            class="carousel-control-prev"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="prev"
-          >
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-          </button>
-          <button
-            class="carousel-control-next"
-            type="button"
-            data-bs-target="#carouselExampleDark"
-            data-bs-slide="next"
-          >
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-          </button>
-        </div>
-      </div>
+    <div className="overflow-hidden ">
 
-      <div class="container py-5">
+      <div id="carouselExample" className="carousel slide">
+        <div className="carousel-inner">
+          {carouselSlides.map((item, index) => (
+            <div
+              key={index}
+              className={`carousel-item ${index === 0 ? "active" : ""}`}
+            >
+              <img src={item.img} alt={item.title} />
+              <div className="carousel-caption d-flex flex-column h-100 align-items-center justify-content-center text-center">
+    <h5>{item.title}</h5>
+    <p>{item.text}</p>
+  </div>
+            </div>
+          ))}
+        </div>
+        {/* controls */}
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="prev"
+        >
+          {`< `}
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExample"
+          data-bs-slide="next"
+        >
+          {`>`}
+        </button>
+      </div>
+      
+
+      <div className="container pb-5">
         <h1 className="text-center py-5">Explore the world with us</h1>
 
-        <div class="row g-4">
-          <div class="col-md-4">
-            <div class="card h-100">
+        <div className="row g-4">
+          <div className="col-md-4">
+            <div className="card h-100">
               <img
                 src="https://images.unsplash.com/photo-1580377968131-bac075a9a3d7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBsdW1iaW5nJTIwc29sdXRpb24lMjBzZXJ2aWNlc3xlbnwwfHwwfHx8MA%3D%3D"
-                class="card-img-top"
+                className="card-img-top"
                 alt="Plumbing Service"
               />
-              <div class="card-body">
-                <h5 class="card-title">Plumbing Services</h5>
-                <p class="card-text">
+              <div className="card-body">
+                <h5 className="card-title">Plumbing Services</h5>
+                <p className="card-text">
                   Expert plumbing solutions: installations, repairs, leak
                   detection, and maintenance.
                 </p>
               </div>
-              <div class="card-footer"></div>
+              <div className="card-footer"></div>
             </div>
           </div>
 
-          <div class="col-md-4">
-            <div class="card h-100">
+          <div className="col-md-4">
+            <div className="card h-100">
               <img
                 src="https://media.istockphoto.com/id/1469656864/photo/electrician-engineer-uses-a-multimeter-to-test-the-electrical-installation-and-power-line.webp?a=1&b=1&s=612x612&w=0&k=20&c=eDdOgrMY7RUNadSSchLwiaDGzHV8vUEt0soJ0Q0Fltk="
-                class="card-img-top"
+                className="card-img-top"
                 alt="Electrical Service"
               />
-              <div class="card-body">
-                <h5 class="card-title">Electrical Services</h5>
-                <p class="card-text">
+              <div className="card-body">
+                <h5 className="card-title">Electrical Services</h5>
+                <p className="card-text">
                   From rewiring to lighting installation, our certified
                   electricians ensure safety and quality.
                 </p>
               </div>
-              <div class="card-footer"></div>
+              <div className="card-footer"></div>
             </div>
           </div>
 
-          <div class="col-md-4">
-            <div class="card h-100">
+          <div className="col-md-4">
+            <div className="card h-100">
               <img
                 src="https://images.unsplash.com/photo-1680503397107-475907e4f3e3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZnVybml0dXJlc3xlbnwwfHwwfHx8MA%3D%3D"
-                class="card-img-top"
+                className="card-img-top"
                 alt="Furniture Service"
               />
-              <div class="card-body">
-                <h5 class="card-title">Furniture Services</h5>
-                <p class="card-text">
+              <div className="card-body">
+                <h5 className="card-title">Furniture Services</h5>
+                <p className="card-text">
                   Assembly, repair, and finishing of all furniture
                   types—delivered with craftsmanship.
                 </p>
               </div>
-              <div class="card-footer"></div>
+              <div className="card-footer"></div>
             </div>
           </div>
         </div>
